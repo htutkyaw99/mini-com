85fbf6c4c5dc67cbc185ee9088dccfc743825321
htutkyaw <htutkyaw22dec99@gmail.com>
2025-03-28 02:30:44 +0300

    lib changed

--- app/_utils/supabase/supabase.ts
+++ app/_utils/supabase/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// Check if the variables are undefined
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key are required.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
